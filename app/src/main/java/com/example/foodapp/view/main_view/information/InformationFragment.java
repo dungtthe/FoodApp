398ed1078a08f5a4b509aa83f9ec3eecccfaff28
Dung Tien The <dungtienthe1920@gmail.com>
2024-05-23 13:56:54 +0700

    up UI profile

--- app/src/main/java/com/example/foodapp/view/main_view/information/InformationFragment.java
+++ app/src/main/java/com/example/foodapp/view/main_view/information/InformationFragment.java
@@ -1,12 +1,16 @@
 package com.example.foodapp.view.main_view.information;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.foodapp.R;
 /**
@@ -50,6 +54,7 @@ public class InformationFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
@@ -61,5 +66,17 @@ public class InformationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_information, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            ImageView imageView=getView().findViewById(R.id.iv_avatar);
+            imageView.setClipToOutline(true);
+        }
+
     }
 }
