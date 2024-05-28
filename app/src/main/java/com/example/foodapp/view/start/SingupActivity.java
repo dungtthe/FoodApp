3bf6c3576e10ab28c414994586167ff5390cd284
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-28 22:40:52 +0700

    StartView: Hoàn thành UI quên mật khẩu, welcome và đăng nhập

---
+++ app/src/main/java/com/example/foodapp/view/start/SingupActivity.java
@@ -0,0 +1,14 @@
+package com.example.foodapp.view.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.foodapp.R;
+public class SingupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singup);
+    }
+}
