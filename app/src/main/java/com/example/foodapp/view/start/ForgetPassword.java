c80554fa405f51142403ca4df38faf1c535c38ba
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-28 21:56:14 +0700

    StartView: Hoàn thành UI quên mật khẩu, welcome và đăng nhập

---
+++ app/src/main/java/com/example/foodapp/view/start/ForgetPassword.java
@@ -0,0 +1,14 @@
+package com.example.foodapp.view.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.foodapp.R;
+public class ForgetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+}
---
+++ app/src/main/java/com/example/foodapp/view/start/Login.java
@@ -0,0 +1,15 @@
+package com.example.foodapp.view.start;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.foodapp.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
