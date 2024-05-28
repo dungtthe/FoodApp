a50a4f5ec307a3e7c05c89d7bfa6d1e13996703c
Dung Tien The <dungtienthe1920@gmail.com>
2024-05-28 23:42:08 +0700

    fix ui welcome

--- app/src/main/java/com/example/foodapp/view/start/WelcomActivity.java
+++ app/src/main/java/com/example/foodapp/view/start/WelcomActivity.java
@@ -3,6 +3,8 @@ package com.example.foodapp.view.start;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import com.example.foodapp.R;
 public class WelcomActivity extends AppCompatActivity {
 
@@ -10,5 +12,6 @@ public class WelcomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcom);
+
     }
 }
