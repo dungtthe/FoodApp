128a92dfad158e2ae081c5ab763fd07bb746e759
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-29 10:14:21 +0700

    KhachHangDTO : Đã tạo

---
+++ app/src/main/java/com/example/foodapp/model/DTO/KhachHangDTO.java
@@ -0,0 +1,73 @@
+package com.example.foodapp.model.DTO;
+
+import android.graphics.Bitmap;
+
+public class KhachHangDTO {
+    private int id;
+    private String hoTen;
+    private String sDT;
+    private String eMail;
+    private boolean daXoa;
+    private String tenTaiKhoan;
+    private String matKhau;
+    private Bitmap hinhAnh;
+
+    //Getter và Setter cho các thuộc tính
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public String getsDT() {
+        return sDT;
+    }
+    public void setsDT(String hoTen) {
+        this.sDT = sDT;
+    }
+
+    public String getMail() {
+        return eMail;
+    }
+    public void setMail(String eMail) {
+        this.hoTen = eMail;
+    }
+
+    public boolean isDaXoa() {
+        return daXoa;
+    }
+    public void setDaXoa(boolean daXoa) {
+        this.daXoa = daXoa;
+    }
+
+    public String gettenTaiKhoan() {
+        return tenTaiKhoan;
+    }
+    public void settenTaiKhoan(String tenTaiKhoan) {
+        this.tenTaiKhoan = tenTaiKhoan;
+    }
+
+    public String getMatKhau() {
+        return matKhau;
+    }
+    public void setMatKhau(String matKhau) {
+        this.matKhau = matKhau;
+    }
+
+    public Bitmap getHinhAnh() {
+        return hinhAnh;
+    }
+    public void setHinhAnh(Bitmap hinhAnh) {
+        this.hinhAnh = hinhAnh;
+    }
+
+
+}
