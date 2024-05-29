870d77fc20f151f2c995aa694742a50b045a15da
Vu Binh <22520129@gm.uit.edu.vn>
2024-05-30 00:30:07 +0700

    Detail_SanPham_For_Home: Hoàn thành UI

--- app/src/main/java/com/example/foodapp/view/main_view/home/Detail_SanPham_For_Home_Activity.java
+++ app/src/main/java/com/example/foodapp/view/main_view/home/Detail_SanPham_For_Home_Activity.java
@@ -3,6 +3,7 @@ package com.example.foodapp.view.main_view.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.foodapp.R;
 
 public class Detail_SanPham_For_Home_Activity extends AppCompatActivity {
 
